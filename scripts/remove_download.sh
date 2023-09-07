1f68808ecc6d900217d99e2fbfee9bacd5039efe
Puja Trivedi <pujatrivedi@Pujas-MacBook-Air.local>
2023-09-06 18:47:57 -0700

    Edited path used in script

--- scripts/remove_download.sh
+++ scripts/remove_download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # # Define the files you want to process
-file1="../_build/html/docs/cluster_annotations/README.html"
-file2="../_build/html/docs/gene_lists/README.html"
-file3="../_build/html/docs/parcellation_annotations/README.html"
+file1="./_build/html/docs/cluster_annotations/README.html"
+file2="./_build/html/docs/gene_lists/README.html"
+file3="./_build/html/docs/parcellation_annotations/README.html"
 
 # Define the sed command
 sed_command='s/<a class="reference download internal" download=""/<a/g; s/<span class="xref download myst"/<span/g'
